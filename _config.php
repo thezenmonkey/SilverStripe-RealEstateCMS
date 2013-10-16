41de4724723d553abd4a699ea9b8044ea2d94c7c
Richard Rudy <rick@nervedesigns.com>
2013-10-16 16:21:26 -0400

    Styling Backend

--- _config.php
+++ _config.php
@@ -1,3 +1,6 @@
 <?php
 
 Director::addRules(50, array('rets' => 'RETS_Controller', 'maintenance' => 'RMSMaintenance'));
+LeftAndMain::require_css('RealEstate/css/realestatecms.css');
+LeftAndMain::require_css('RealEstate/css/jquery.mobile.flatui.css');
+LeftAndMain::require_css('http://fonts.googleapis.com/css?family=Lato:300,700,100italic');
