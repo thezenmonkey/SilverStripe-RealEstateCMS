2eae212ad3d6a803a9f8fcaba2ec51af7b0773fc
Richard Rudy <rick@nervedesigns.com>
2013-11-15 18:47:47 -0500

    Resync

--- _config.php
+++ _config.php
@@ -1,8 +1,3 @@
 <?php
 
 Director::addRules(50, array('rets' => 'RETS_Controller', 'maintenance' => 'RMSMaintenance'));
-LeftAndMain::require_css('RealEstate/css/realestatecms.css');
-LeftAndMain::require_css('RealEstate/css/jquery.mobile.flatui.css');
-LeftAndMain::require_css('http://fonts.googleapis.com/css?family=Lato:300,700,100italic');
-
-Config::inst()->update("ListingsPage", "excluded_children", array("Listing"));
