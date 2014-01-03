dd42cbf310fbd27513a2af2ac67cb657e0c5323b
Richard Rudy <rick@designplusawesome.com>
2014-01-03 14:48:33 -0500

    Added Blog Community Extension

--- code/DataExtension/RMSBlogExtension.php
+++ code/DataExtension/RMSBlogExtension.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Extensions for connecting SilverStripe Blog module and RMS	
+ * @package Real Estate Manaagemtn System
+ * @author Richard Rudy @thezenmonkey http://designplusawesome.com
+ */
+
+/**
+ * Extend BlogEntry to inlude community
+ */
 
 class RMSBlogExtension extends DataExtension {
 	/*
@@ -14,11 +23,17 @@ class RMSBlogExtension extends DataExtension {
 	private static $has_many = array(
 		
 	);
+	*/
+	private static $many_many = array(
+		"Communities" => "Community"
+	);
 	
 	public function updateCMSFields(FieldList $fields) {
 		
+		$fields->insertAfter(TreeMultiselectField::create("Communities","Communities","MunicipalityPage","ID","Title"), "Tags");
+		
 	}
-*/
+
 	
 	/**
 	 * Get Listings based on tags
@@ -31,7 +46,8 @@ class RMSBlogExtension extends DataExtension {
 	 * @todo Actually Test it
 	 */
 	
-	public function GetListings($getCity, $getHood, $limit = null) {
+	/*
+public function GetListings($getCity, $getHood, $limit = null) {
 		$tags = preg_split(" *, *", trim($this->owner->Tags));
 		
 		$filter = array();
@@ -46,4 +62,34 @@ class RMSBlogExtension extends DataExtension {
 		return $listings->count() ? $listing->limit($limit) : false;
 		
 	}
+*/
+}
+
+/**
+ * Extend Community to inlude BlogEntry
+ */
+
+class BlogCommunityExtension extends DataExtension {
+	
+	private static $db = array(
+		
+	);
+	
+	private static $has_one = array(
+		
+	);
+	
+	private static $has_many = array(
+		
+	);
+	
+	private static $belongs_many_many = array(
+		"BlogEntries" => "BlogEntry"
+	);
+	
+	/*
+public function updateCMSFields(FieldList $fields) {
+		
+	}
+*/
 }
